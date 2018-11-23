64115c3777d6c6d2369452b44f7f2bad6512d771
stodipo <odipo87@gmail.com>
2018-11-23 09:50:17 +0300

    Included multiple arguements to the function turtle with length and angle

--- turtle_game.py
+++ turtle_game.py
@@ -3,14 +3,21 @@ import turtle
 my_turtle = turtle.Turtle()
 
 
-def square():
+def square(length, angle):
 	#This part of the code draws the square
-	my_turtle.forward(100)
-	my_turtle.left(90)
-	my_turtle.forward(100)
-	my_turtle.left(90)
-	my_turtle.forward(100)
-	my_turtle.left(90)
-	my_turtle.forward(100)
-	my_turtle.left(90)
-	
+	my_turtle.forward(length)
+	my_turtle.left(angle)
+	my_turtle.forward(length)
+	my_turtle.left(angle)
+	my_turtle.forward(length)
+	my_turtle.left(angle)
+	my_turtle.forward(length)
+	my_turtle.left(angle)
+
+#Adding multiple arguements to functions
+square(250, 90)
+square(200, 90)
+square(150, 90)
+square(100, 90)
+square(50, 90)
+square(25, 90)
